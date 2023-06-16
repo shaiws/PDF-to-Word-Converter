598fb9336d39902ba6bb4d85cbfa5bb93c57529e
Shai Michaeli <shaiws2@gmail.com>
2023-06-16 11:34:17 +0300

    Reduce exe file size

--- converter.py
+++ converter.py
@@ -1,19 +1,19 @@
-import os
-import subprocess
-import argparse
+from os import environ, path,pathsep, remove, name,startfile
+from subprocess import call
+from argparse import ArgumentParser
 from pdf2image import convert_from_path
 from docx import Document
 from docx.shared import Inches
 from tqdm import tqdm
-import tkinter as tk
-from tkinter import filedialog, simpledialog, ttk
-
+from tkinter import filedialog, simpledialog, ttk, Tk
+from sys import executable
 
 def pdf_to_images(pdf_path):
     """
     Function to convert PDF to images
     """
     return convert_from_path(pdf_path)
+
 
 
 def create_docx_from_images(images, docx_path, width, progress=None, root=None):
@@ -26,7 +26,7 @@ def create_docx_from_images(images, docx_path, width, progress=None, root=None):
         img_filename = f'image_{i}.png'
         images[i].save(img_filename)  # saving images
         doc.add_picture(img_filename, width=Inches(width))  # Add image to the docx and adjust the size as per your requirement
-        os.remove(img_filename)  # Remove image file after using
+        remove(img_filename)  # Remove image file after using
         if progress:
             progress['value'] += 100/len(images)
             if root:
@@ -47,18 +47,18 @@ def open_file(path):
     """
     Function to open a file depending on the OS
     """
-    if os.name == 'nt':  # for windows
-        os.startfile(path)
+    if name == 'nt':  # for windows
+        startfile(path)
     else:
-        opener = "open" if os.name == "posix" else "xdg-open"  # for mac and linux respectively
-        subprocess.call([opener, path])
+        opener = "open" if name == "posix" else "xdg-open"  # for mac and linux respectively
+        call([opener, path])
 
 
 def main():
     """
     Main function to handle arguments
     """
-    parser = argparse.ArgumentParser(description='Convert a PDF to a Word document with each page as an image.')
+    parser = ArgumentParser(description='Convert a PDF to a Word document with each page as an image.')
     parser.add_argument('pdf_path', nargs='?', default=None, help='The path to the input PDF file')
     parser.add_argument('docx_path', nargs='?', default=None, help='The path to output the Word document')
     parser.add_argument('-w', '--width', type=float, default=6.0,
@@ -70,7 +70,7 @@ def main():
         pdf_to_docx(args.pdf_path, args.docx_path, args.width)
         open_file(args.docx_path)
     else:
-        root = tk.Tk()
+        root = Tk()
         root.withdraw()  # hide the main window
 
         pdf_path = filedialog.askopenfilename(title="Select PDF file",
